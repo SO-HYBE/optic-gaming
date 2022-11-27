2d772ebc88ffed3565ece64f51de8ed5c1bb54b5
MODARA3A <sohaibibrahim14@gmail.com>
2022-11-27 09:02:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img className='logo' src="https://www.seekpng.com/png/full/183-1837044_optic-gaming-logo-png-optic-gaming-logo-white.png" alt="Optic Gaming Logo Png - Optic Gaming Logo White@seekpng.com"/>
+      <div className="nav-bar">
+        <li className="items">
+          <a href="https://opticgaming.com/about">about</a>
+          <br />
+          <a href="https://opticgaming.com/schedule">schedule</a>
+          <br />
+          <a href="https://opticgaming.com/teams">teams</a>
+          <br />
+          <a href="https://opticgaming.com/creators">creators</a>
+          <br />
+          <a href="https://opticgaming.com/partners">partners</a>
+          <br />
+          <a href="https://opticgaming.com/news">news</a>
+          <br />
+          <a href="https://shop.opticgaming.com/">store</a>
+          <br />
+          <a className='join' href="https://nation.opticgaming.com/">Join the nation</a>
+          <br />
+          
+        </li>
+      </div>
       </header>
     </div>
   );
